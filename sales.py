9e6e63840bcebf1997e81186999ce775b38536dd
Jack Daniels <john@gmail.com>
2026-02-23 18:05:05 +0530

    Add sales.py and sales2.py

---
+++ sales.py
@@ -0,0 +1,12 @@
+
+
+def calculate_pay(tax , salary):
+    return tax*salary
+
+
+def gross_profit(cost_price , selling_price):
+    return selling_price - cost_price
+
+
+print(gross_profit.__class__)
+
---
+++ sales2.py
@@ -0,0 +1,8 @@
+
+
+from sales import calculate_pay , gross_profit
+import sys
+
+
+print(calculate_pay(12,3000))
+print(gross_profit(2300,3000))
